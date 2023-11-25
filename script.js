d28602f8c3a8749d42de4ae274cc0a62cf8fbc3e
arneBersch <97836617+arneBersch@users.noreply.github.com>
2023-11-25 23:32:49 +0100

    added highscore display

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ const drawing_interval = 20;
 var oil_x = 800;
 var oil_y;
 var score;
+var highscore;
 
 function init(){
     canvas = document.getElementById('canvas');
@@ -24,6 +25,11 @@ function init(){
     context = canvas.getContext('2d');
     voltage_input = document.getElementById('voltage_input');
     voltage_input.max = max_voltage;
+    highscore = localStorage.getItem('highscore');
+    if (highscore == 'null' || highscore == null){
+        highscore = 0;
+        localStorage.setItem('highscore', highscore);
+    }
     stop_game();
 }
 
@@ -33,6 +39,7 @@ function start_game(){
     rect_x = (-1 * rect_width);
     rect_y = (0.5 * canvas_height);
     oil_y = canvas_height / 2;
+    voltage_input.value = (max_voltage / 2);
     voltage_input.disabled = false;
     interval = setInterval(draw_circle, drawing_interval);
 }
@@ -43,32 +50,26 @@ function stop_game(){
     voltage_input.disabled = true;
     context.font = '50px monospace';
     context.textAlign = 'center';
-    context.fillStyle = 'white'
-    context.fillText('Millikan-Masters', (canvas_width / 2), 300);
+    context.fillStyle = 'white';
+    if (score > highscore){
+        highscore = score;
+        localStorage.setItem('highscore', highscore);
+        context.fillText(('New Highscore: ' + highscore), (canvas_width / 2), 300);
+    } else if (score > 0){
+        context.fillText(('Score: ' + score), (canvas_width / 2), 300);
+    } else {
+        context.fillText('Millikan-Masters', (canvas_width / 2), 300);
+    }
     context.fillText('Klick to start', (canvas_width / 2), 500);
     canvas.onclick = start_game;
 }
 
 function draw_circle(){
-    context.clearRect(0, 0, canvas_width, canvas_height)
-    var voltage = voltage_input.value;
-    oil_dy = (voltage - (max_voltage / 2)) * voltage_sensitivity;
-    oil_y += oil_dy;
-    if ((oil_y <= oil_radius) || (oil_y >= (canvas_height - oil_radius))){
-        stop_game();
-    }
-    if (rect_x > canvas_width){
-        rect_x = -1 * rect_width;
-        rect_y = Math.floor(Math.random() * (canvas_height - rect_height))
-    } else {
-        rect_x += ((rect_start_speed + score) / 600);
-        if ((rect_x < (oil_x + oil_radius)) && ((rect_x + rect_width) > (oil_x - oil_radius))){
-            if ((rect_y < (oil_y + oil_radius)) && ((rect_y + rect_height) > (oil_y - oil_radius))){
-                stop_game();
-            }
-        }
-    }
+    context.clearRect(0, 0, canvas_width, canvas_height);
     score += 1;
+    var voltage = voltage_input.value;
+    let oil_dy = (voltage - (max_voltage / 2)) * voltage_sensitivity;
+    oil_y += oil_dy;
     context.strokeStyle = 'white';
     context.lineWidth = 10;
     context.strokeRect(rect_x, rect_y, rect_width, rect_height);
@@ -79,6 +80,20 @@ function draw_circle(){
     context.fill();
     context.font = '30px monospace';
     context.textAlign = 'right';
-    context.fillStyle = 'white'
-    context.fillText(score, (canvas_width - 10), 40);
+    context.fillStyle = 'white';
+    context.fillText((score + ' HS: ' + highscore), (canvas_width - 10), 40);
+    if ((oil_y <= oil_radius) || (oil_y >= (canvas_height - oil_radius))){
+        stop_game();
+    }
+    if (rect_x > canvas_width){
+        rect_x = -1 * rect_width;
+        rect_y = Math.floor(Math.random() * (canvas_height - rect_height));
+    } else {
+        rect_x += ((rect_start_speed + score) / 600);
+        if ((rect_x < (oil_x + oil_radius)) && ((rect_x + rect_width) > (oil_x - oil_radius))){
+            if ((rect_y < (oil_y + oil_radius)) && ((rect_y + rect_height) > (oil_y - oil_radius))){
+                stop_game();
+            }
+        }
+    }
 }
